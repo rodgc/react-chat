722a4f81dd58083f57a2035d73d09110ffef9928
rodgc <rod.gc@hotmail.com>
2020-05-27 11:53:54 -0500

    Initial Commit

---
+++ typings.d.ts
@@ -0,0 +1,10 @@
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
+
+declare module 'socket.io-client';
+declare module 'react-emojione';
+declare module 'linkifyjs/react';
+declare module '@microlink/react';
+declare module 'get-urls';
